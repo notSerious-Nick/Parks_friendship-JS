4a31416afaaf99523e8e9d10639ea5f5d4f83fd7
Nick Park <149343667+notSerious-Nick@users.noreply.github.com>
2024-10-01 19:29:15 +0900

    finish login section with JS
    Next step will be background

---
+++ js/login.js
@@ -0,0 +1,31 @@
+const loginWindow = document.querySelector(".login_window");
+const loginInput = document.querySelector(".login_window_form-input");
+const btn = document.querySelector(".login_window_form-btn");
+const nameInput = document.querySelector(".welcome_input")
+
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginWindow.style.display = "none";
+    const name = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, name);
+    paintGreetings(name);
+}
+
+function paintGreetings(username){
+    nameInput.innerHTML = `What's up <br/> ${username}`;
+}
+const savedName = localStorage.getItem(USERNAME_KEY);
+
+
+// I cannot understand why below code require give me any situation when this code occur
+if (savedName === null) {
+    loginWindow.addEventListener("submit", onLoginSubmit);
+  }
+
+if(savedName !== null){
+    loginWindow.style.display = "none";
+    paintGreetings(savedName);
+}
+btn.addEventListener("click", onLoginSubmit);
