b61afad41c90a2cb6f26fd9c339c81c6777e569f
Nick Park <149343667+notSerious-Nick@users.noreply.github.com>
2024-10-05 19:00:13 +0900

    finshed all except todolist
    Spending 45mins more to fix minor problems such as music( when the music ended, next music did not play.

--- js/weather.js
+++ js/weather.js
@@ -9,9 +9,32 @@ function onGeoSuccess(postion){
     const lat = postion.coords.latitude;
     const lon = postion.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    
+    const weatherImg = document.createElement("i");
+
     fetch(url).then((response) => response.json()).then((data) =>{
         userLocation.innerHTML = data.name;
+        weatherImg.classList.toggle("fa-solid");
+        if("Thunderstorm" == data.weather[0].main){
+            weatherImg.classList.toggle("fa-bolt");
+        } else if("Drizzle" == data.weather[0].main){
+            weatherImg.classList.toggle("fa-cloud-rain");
+        }
+        else if("Rain" == data.weather[0].main){
+            weatherImg.classList.toggle("fa-cloud-showers-heavy");
+        }
+        else if("Snow" == data.weather[0].main){
+            weatherImg.classList.toggle("fa-snowflake");
+        }
+        else if("Atmosphere" == data.weather[0].main){
+            weatherImg.classList.toggle("fa-smog");
+        }
+        else if("Clear" == data.weather[0].main){
+            weatherImg.classList.toggle("fa-sun");
+        }
+        else{
+            weatherImg.classList.toggle("fa-cloud");
+        }
+        document.querySelector(".weather_img").appendChild(weatherImg);
         weather.innerHTML = `weather: ${data.weather[0].main}`
         temperture.innerHTML = `temperture: ${data.main.temp}°C`
     })
