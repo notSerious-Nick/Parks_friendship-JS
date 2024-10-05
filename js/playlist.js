0bc54dc876f8b732c9485dc099b25917263e4590
Nick Park <149343667+notSerious-Nick@users.noreply.github.com>
2024-10-05 12:57:27 +0900

    playlist data, img downloaded, those in JS rn
    1h 30min works

---
+++ js/playlist.js
@@ -0,0 +1,39 @@
+let musics = [
+    {
+        path: "musics/good days.mp3",
+        cover: "img/playlists_albums/good days.jpg",
+        name: "good days",
+        artisit: "Keshi",
+    },
+    {
+        path: "musics/Jungle.mp3",
+        cover: "img/playlists_albums/jungle.jpg",
+        name: "Jungle",
+        artisit: "Drake",
+    },
+    {
+        path: "musics/Like That.mp3",
+        cover: "img/playlists_albums/like that.jpg",
+        name: "Like That",
+        artisit: "Keshi",
+    },
+    {
+        path: "musics/Lonely (with benny blanco).mp3",
+        cover: "img/playlist_albums/lonely.jpg",
+        name: "Lonely (with benny blanco)",
+        artisit: "Justin Bieber, benny blanco",
+    },
+    {
+        path: "musics/Open Arms (feat. Travis Scott).mp3",
+        cover: "img/playlist_albums/open arms.jpg",
+        name: "Open Arms (feat. Travis Scott)",
+        artisit: "SZA, Travis Scott",
+    },
+    {
+        path: "musics/White Ferrari.mp3",
+        cover: "img/playlist_albums/white ferrari.jpg",
+        name: "White Ferrari",
+        artisit: "Frank Ocean",
+    }
+    
+]
